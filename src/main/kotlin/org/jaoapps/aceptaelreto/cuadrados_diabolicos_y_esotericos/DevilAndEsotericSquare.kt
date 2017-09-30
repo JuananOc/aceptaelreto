f18788eb0bec3ae11fa40c64561b5429d38a8e94
Javier Sánchez Alonso <javier.s7a@gmail.com>
2017-09-30 22:00:21 +0200

    Cupiers fixes 101 - Cuadrados diabólicos y esotérico (WIP)

--- src/main/kotlin/org/jaoapps/aceptaelreto/cuadrados_diabolicos_y_esotericos/DevilAndEsotericSquare.kt
+++ src/main/kotlin/org/jaoapps/aceptaelreto/cuadrados_diabolicos_y_esotericos/DevilAndEsotericSquare.kt
@@ -1,140 +1,77 @@
 package org.jaoapps.aceptaelreto.cuadrados_diabolicos_y_esotericos
 
+import java.util.*
 
-class Square {
 
-    // TODO: Hacer privadas con getter
-    public val size: Int
-    public val data: List<Int>
-    public val square: List<List<Int>>
-    public val magicConstant: Int
-    public val magicConstant2: Int
-    private val squareLimit: Int
-        get() {
-            val squareLimit = size - 1
-            return squareLimit
-        }
+class Square(val dimension: Int, celdas: String) {
 
-    constructor(size: Int, data: String) {
-        this.size = size
-        this.data = getDataSet(data)
-        this.square = createSquare()
-        this.magicConstant = this.square[0].sum()
-        this.magicConstant2 = 4 * magicConstant / size
-    }
+    val square: List<List<Int>>
+    private val squareLimit: Int = dimension - 1
+    private val magicConstant: Int
+    private val magicConstant2: Int
 
-    // TODO: no pasar el data
-    internal constructor(square: List<List<Int>>, data: String) {
-        this.square = square
-        this.size = square.size
-        this.data = getDataSet(data)
-        this.magicConstant = this.square[0].    sum()
-        this.magicConstant2 = 4 * magicConstant / size
-    }
+    init {
+        val cells = celdas.split(" ").toIntList()
 
-    private fun getDataSet(data: String): ArrayList<Int> {
-        val listSplitedBySpaces = data.split(" ")
-        return listSplitedBySpaces.toIntList()
-    }
+        this.square = ArrayList(dimension)
 
-    private fun createSquare(): List<List<Int>> {
-        val square = ArrayList<ArrayList<Int>>(size)
-
-        for (squareField in this.data) {
-            val fieldIndex = this.data.indexOf(squareField)
-
-            if (hasToCreateNewRow(fieldIndex)) {
-                square.add(createRow(fieldIndex))
+        for ((index, _) in cells.withIndex()) {
+            if (hasToCreateNewRow(index)) {
+                square.add(cells.subList(index, index + dimension))
             }
         }
-        return square
+
+        this.magicConstant = square[0].sum()
+        this.magicConstant2 = 4 * magicConstant / dimension
     }
 
-    private fun hasToCreateNewRow(fieldIndex: Int) = fieldIndex % size == 0
+    private fun List<String>.toIntList() = this.map { it.toInt() }
 
-    private fun createRow(fieldIndex: Int): ArrayList<Int> {
-        val row = ArrayList<Int>()
+    private fun hasToCreateNewRow(cellIndex: Int) = cellIndex % dimension == 0
 
-        val squareFileSize = fieldIndex + size
-        for (i in fieldIndex until squareFileSize) {
-            row.add(this.data[i])
-        }
+    fun isDiabolic() = rowsAreEqualsToMagicConstant() && columsAreEqualsToMagicConstant() && diagonalsAreEqualsToMagicConstant()
 
-        return row;
-    }
+    private fun diagonalsAreEqualsToMagicConstant() = leftToRightDiagonalIsEqualsToMagicConstant() && rightToLeftIsEqualsToMagicConstant()
 
-    fun isDiabolic(): Boolean {
-        if (! rowsAreEqualsToMagicConstant()) return false
-        if (! columsAreEqualsToMagicConstant()) return false
-        if (! diagonalsAreEqualsToMagicConstant()) return false
+    private fun rowsAreEqualsToMagicConstant() = square.all { it.sum() == magicConstant }
 
-        return true;
-    }
+    private fun columsAreEqualsToMagicConstant() = (0 until dimension).all { square[it].sum() == magicConstant }
 
-    private fun diagonalsAreEqualsToMagicConstant() = leftToRightDiagonalIsEqualsToMagicConstant() || rightToLeftIsEqualsToMagicConstant()
+    private fun leftToRightDiagonalIsEqualsToMagicConstant() = (0 until dimension).sumBy { square[it][it] } == magicConstant
 
-    private fun rowsAreEqualsToMagicConstant(): Boolean {
-        for (row in square) {
-            if (row.sum() == magicConstant) return true
-        }
-        return false
-    }
-
-    private fun columsAreEqualsToMagicConstant(): Boolean {
-        for (columnIndex in 0 until square.size) {
-            var sum = 0
-            for (fileIndex in 0 until square.size) {
-                sum += square[columnIndex][fileIndex]
-            }
-            if (sum == magicConstant) return true
-        }
-        return false
-    }
-
-    private fun leftToRightDiagonalIsEqualsToMagicConstant(): Boolean {
-        // calculate diagonal from [0,0] to [size, size]
-        var sum = 0
-        for (diagonalIndex in 0 until square.size) {
-            sum += square[diagonalIndex][diagonalIndex]
-        }
-        if (sum == magicConstant) return true
-        return false
-    }
-
-    private fun rightToLeftIsEqualsToMagicConstant(): Boolean {
-        var sum = 0
-        for (diagonalIndex in (square.size - 1) downTo 0) {
-            sum += square[diagonalIndex][diagonalIndex]
-        }
-
-        if (sum == magicConstant) return true
-        return false
-    }
+    private fun rightToLeftIsEqualsToMagicConstant() = (dimension - 1 downTo 0).sumBy { square[it][it] } == magicConstant
 
     fun isEsoteric(): Boolean {
-        if (! isDiabolic()) return false
-        if (! containsAllNaturalNumber()) return false
-        if (! sumOfCornersAreEqualsToConstantMagic2()) return false
+        if (!isDiabolic()) return false
+        if (!containsAllNaturalNumber()) return false
+        if (!sumOfCornersAreEqualsToConstantMagic2()) return false
 
         if (isEven()) {
-            val centralRightIndex = size / 2
+            val centralRightIndex = dimension / 2
             val centralLeftIndex = centralRightIndex - 1
 
-            if (! sumOfLateralFieldsAreEqualsToDoubleOfMagicConstant2(centralRightIndex, centralLeftIndex)) return false
-            if (! sumCentralFieldsAreEqualsToMagicConstant2(centralRightIndex, centralLeftIndex)) return false
+            if (!sumOfLateralFieldsAreEqualsToDoubleOfMagicConstant2(centralRightIndex, centralLeftIndex)) return false
+            if (!sumCentralFieldsAreEqualsToMagicConstant2(centralRightIndex, centralLeftIndex)) return false
 
         } else {
-            val centralIndex = size / 2
-            if (! sumLateralFieldsAreEqualsToMagicConstant2(centralIndex)) return false
-            if (! centralFieldMultipliedBy4AreEqualsToMagicConstant2(centralIndex)) return false
+            val centralIndex = dimension / 2
+            if (!sumLateralFieldsAreEqualsToMagicConstant2(centralIndex)) return false
+            if (!centralFieldMultipliedBy4AreEqualsToMagicConstant2(centralIndex)) return false
         }
 
         return true
     }
 
     private fun containsAllNaturalNumber(): Boolean {
-        val naturalNumbers = (1..size * size).toList()
-        return data.sorted().equals(naturalNumbers)
+        val naturalNumbers = (1..dimension * dimension).toList()
+
+        var total = emptyList<Int>()
+
+        for (list in square) {
+            total += list
+        }
+
+        return total.sorted().equals(naturalNumbers)
     }
 
     private fun sumOfCornersAreEqualsToConstantMagic2(): Boolean {
@@ -144,7 +81,7 @@ class Square {
         return false
     }
 
-    private fun isEven() = size % 2 == 0
+    private fun isEven() = dimension % 2 == 0
 
     private fun sumOfLateralFieldsAreEqualsToDoubleOfMagicConstant2(centralRightIndex: Int, centralLeftIndex: Int): Boolean {
         val sumLateralFields = square[0][centralRightIndex] + square[0][centralLeftIndex] +
@@ -181,15 +118,4 @@ class Square {
         if (isDiabolic()) return "DIABOLICO"
         return "NO"
     }
-}
-
-// TODO: por qué no funciona si pongo <String> después del fun
-// private fun <String> List<String>.toIntList(): ArrayList<Int> {
-private fun List<String>.toIntList(): ArrayList<Int> {
-    val new = ArrayList<Int>()
-
-    for (element in this) {
-        new.add(element.toInt())
-    }
-    return new
 }
--- src/test/kotlin/org/jaoapps/aceptaelreto/cuadrados_diabolicos_y_esotericos/DevilAndEsotericSquareTest.kt
+++ src/test/kotlin/org/jaoapps/aceptaelreto/cuadrados_diabolicos_y_esotericos/DevilAndEsotericSquareTest.kt
@@ -1,6 +1,6 @@
 package org.jaoapps.aceptaelreto.cuadrados_diabolicos_y_esotericos
 
-import org.junit.Assert
+import org.junit.Assert.*
 import org.junit.Test
 
 class DevilAndEsotericSquareTest {
@@ -10,64 +10,56 @@ class DevilAndEsotericSquareTest {
         val square = Square(2, "1 2 3 4")
         val expectedSquare = listOf(listOf(1, 2), listOf(3, 4))
 
-        Assert.assertEquals(square.size, 2)
-        Assert.assertEquals(square.square, expectedSquare)  // TODO: sobreescribir el equals del square para que sea como el equals de List<>
+        assertEquals(square.dimension, 2)
+        assertEquals(square.square, expectedSquare)  // TODO: sobreescribir el equals del square para que sea como el equals de List<>
 
         val square2 = Square(3, "1 2 3 4 5 6 7 8 9")
         val expectedSquare2 = listOf(listOf(1, 2, 3), listOf(4, 5, 6), listOf(7, 8, 9))
 
-        Assert.assertEquals(square2.size, 3)
-        Assert.assertEquals(square2.square, expectedSquare2)
+        assertEquals(square2.dimension, 3)
+        assertEquals(square2.square, expectedSquare2)
     }
 
     @Test
     fun is_diabolic() {
-        val diabolicSquare = Square(listOf(listOf(28, 21, 26), listOf(23, 25, 27), listOf(24, 29, 22)), "28 21 26 23 25 27 24 29 22")
-        Assert.assertTrue(diabolicSquare.isDiabolic())
+        val diabolicSquare = Square(3, "28 21 26 23 25 27 24 29 22")
+        assertTrue(diabolicSquare.isDiabolic())
 
-        val not_diabolic = Square(listOf(listOf(1, 2), listOf(3, 4)), "1 2 3 4")
-        Assert.assertFalse(not_diabolic.isDiabolic())
+        val notDiabolic = Square(2, "1 2 3 4")
+        assertFalse(notDiabolic.isDiabolic())
     }
 
     @Test
     fun is_esoteric() {
-        val not_diabolic = Square(listOf(listOf(1, 2), listOf(3, 4)), "1 2 3 4")
-        Assert.assertFalse(not_diabolic.isEsoteric())
+        val notDiabolic = Square(2, "1 2 3 4")
+        assertFalse(notDiabolic.isEsoteric())
 
-        val diabolicNotEsoteric = Square(listOf(listOf(28, 21, 26), listOf(23, 25, 27), listOf(24, 29, 22)), "28 21 26 23 25 27 24 29 22")
-        Assert.assertFalse(diabolicNotEsoteric.isEsoteric())
+        val diabolicNotEsoteric = Square(3, "28 21 26 23 25 27 24 29 22")
+        assertFalse(diabolicNotEsoteric.isEsoteric())
 
-        val oddEsoteric = Square(listOf(listOf(4, 9, 2), listOf(3, 5, 7), listOf(8, 1, 6)), "4 9 2 3 5 7 8 1 6")
-        Assert.assertTrue(oddEsoteric.isEsoteric())
+        val oddEsoteric = Square(3, "4 9 2 3 5 7 8 1 6")
+        assertTrue(oddEsoteric.isEsoteric())
 
-        val evenEsoteric = Square(listOf(
-                listOf(1, 63, 62, 4, 5, 59, 58, 8),
-                listOf(56, 10, 11, 53, 52, 14, 15, 49),
-                listOf(48, 18, 19, 45, 44, 22, 23, 41),
-                listOf(25, 39, 38, 28, 29, 35, 34, 32),
-                listOf(33, 31, 30, 36, 37, 27, 26, 40),
-                listOf(24, 42, 43, 21, 20, 46, 47, 17),
-                listOf(16, 50, 51, 13, 12, 54, 55, 9),
-                listOf(57, 7, 6, 60, 61, 3, 2, 64)), "1 63 62 4 5 59 58 8 56 10 11 53 52 14 15 49 48 18 19 45 44 22 23 41 25 39 38 28 29 35 34 32 33 31 30 36 37 27 26 40 24 42 43 21 20 46 47 17 16 50 51 13 12 54 55 9 57 7 6 60 61 3 2 64")
-        Assert.assertTrue(evenEsoteric.isEsoteric())
+        val evenEsoteric = Square(8, "1 63 62 4 5 59 58 8 56 10 11 53 52 14 15 49 48 18 19 45 44 22 23 41 25 39 38 28 29 35 34 32 33 31 30 36 37 27 26 40 24 42 43 21 20 46 47 17 16 50 51 13 12 54 55 9 57 7 6 60 61 3 2 64")
+        assertTrue(evenEsoteric.isEsoteric())
     }
 
     @Test
     fun diabolic_and_esoteric_square() {
         val esoteric = Square(3, "4 9 2 3 5 7 8 1 6")
-        Assert.assertEquals(esoteric.isDevilEsotericSquare(), "ESOTERICO")
+        assertEquals(esoteric.isDevilEsotericSquare(), "ESOTERICO")
 
-        val nothing  = Square(2, "1 2 3 4")
-        Assert.assertEquals(nothing.isDevilEsotericSquare(), "NO")
+        val nothing = Square(2, "1 2 3 4")
+        assertEquals(nothing.isDevilEsotericSquare(), "NO")
 
         val anotherEsoteric = Square(4, "16 3 2 13 5 10 11 8 9 6 7 12 4 15 14 1")
-        Assert.assertEquals(anotherEsoteric.isDevilEsotericSquare(), "ESOTERICO")
+        assertEquals(anotherEsoteric.isDevilEsotericSquare(), "ESOTERICO")
 
         val diabolic = Square(3, "28 21 26 23 25 27 24 29 22")
-        Assert.assertEquals(diabolic.isDevilEsotericSquare(), "DIABOLICO")
+        assertEquals(diabolic.isDevilEsotericSquare(), "DIABOLICO")
 
         val anotherNothing = Square(3, "2 8 1 6 3 5 7 4 9")
-        Assert.assertEquals(anotherNothing.isDevilEsotericSquare(), "NO")
+        assertEquals(anotherNothing.isDevilEsotericSquare(), "NO")
     }
 
 }
